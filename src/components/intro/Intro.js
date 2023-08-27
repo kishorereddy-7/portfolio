7cb52c1c0f9c25bdac2fec022e80f24e527d1b23
Kishore <guttikondakishorereddy@gmail.com>
2023-08-27 23:59:37 +0530

    intro-theme

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
 import { personalDetails } from "../../constant";
 import useTypedSuperPower from "../customHooks/useTypedSuperPower";
 import "./Intro.css";
+
+const runImageSrc = new URL("../assets/run.jpg", import.meta.url);
+const playImageSrc = new URL("../assets/play.jpg", import.meta.url);
+const winImageSrc = new URL("../assets/win.jpg", import.meta.url);
 
 const iam = [
   "a Frontend Developer",
@@ -8,19 +13,52 @@ const iam = [
   "a Angular Developer",
 ];
 
+const titleColors = [
+  "text-sky-600",
+  "text-orange-600",
+  "text-green-600",
+  "text-red-600",
+  "text-pink-600",
+  "text-blue-600",
+  "text-yellow-600",
+  "text-grey-600",
+  "text-lime-600",
+  "text-emerald-600",
+];
+
+const getRandomIndex = () => Math.floor(Math.random() * 10);
+
 const Intro = () => {
   const { typedSuperPower, selectedSuperPower } = useTypedSuperPower(iam);
+  const [titleColor, setTitleColor] = useState(titleColors[getRandomIndex()]);
+
+  useEffect(() => {
+    if (!typedSuperPower) {
+      setTitleColor(titleColors[getRandomIndex()]);
+    }
+  }, [typedSuperPower]);
+
   return (
-    <div className="intro-img flex justify-center items-center flex-col text-center text-white">
-      <h1 className="text-7xl header-start">
-        {personalDetails.firstName + " " + personalDetails.lastName}
-      </h1>
-      <h1 className="text-4xl my-8">
-        I am{" "}
-        <span className="blinking-cursor" aria-label={selectedSuperPower}>
-          {typedSuperPower}
-        </span>
-      </h1>
+    <div className="flex flex-col intro">
+      <div className="flex justify-around my-8">
+        <img className="intro-img" src={runImageSrc} alt="run" />
+        <img className="intro-img" src={playImageSrc} alt="play" />
+        <img className="intro-img" src={winImageSrc} alt="win" />
+      </div>
+      <div className="text-center text-sk my-8 text-black">
+        <h1 className="text-7xl header-start">
+          {personalDetails.firstName + " " + personalDetails.lastName}
+        </h1>
+        <h1 className="text-4xl my-8 font-extrabold">
+          I am{" "}
+          <span
+            className={"blinking-cursor" + " " + titleColor}
+            aria-label={selectedSuperPower}
+          >
+            {typedSuperPower}
+          </span>
+        </h1>
+      </div>
     </div>
   );
 };
